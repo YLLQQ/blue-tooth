c3b3d93938ceee8485ecb6f19d3018b3036ae07f
EL <ysh1059@163.com>
2019-11-01 22:37:00 +0800

    添加打开或关闭蓝牙按钮

--- app/src/main/java/self/yang/application/MainActivity.kt
+++ app/src/main/java/self/yang/application/MainActivity.kt
@@ -1,10 +1,11 @@
 package self.yang.application
 
+import android.bluetooth.BluetoothAdapter
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 const val EXTRA_MESSAGE = "self.yang.application.MESSAGE"
 
@@ -13,6 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * 调用系统页面打开蓝牙
+     */
+    fun openBluetooth(view: View) {
+        startActivityForResult(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
+    }
+
+    /**
+     * 关闭蓝牙
+     */
+    fun closeBluetooth(view: View) {
+        var bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (null != bluetoothAdapter && bluetoothAdapter.isEnabled) {
+            bluetoothAdapter.disable()
+        }
     }
 
     /**
@@ -26,8 +45,8 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
-        startActivity(intent)
 
+        startActivity(intent)
     }
 
 }
