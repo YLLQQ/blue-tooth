55572b657da061daf69363450bc8104749cecc8f
yangguoqing <ysh1059@163.com>
2019-11-02 09:56:27 +0800

    显示用户授权状态

--- app/src/main/java/self/yang/application/MainActivity.kt
+++ app/src/main/java/self/yang/application/MainActivity.kt
@@ -1,11 +1,13 @@
 package self.yang.application
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,6 +20,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * 用户授权选择结果回调
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (Activity.RESULT_CANCELED == resultCode) {
+            callToast("用户拒绝授权")
+        } else if (Activity.RESULT_OK == resultCode) {
+            callToast("用户授权成功")
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    /**
+     * 调用Toast打印提示信息
+     */
+    private fun callToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
     }
 
     /**
